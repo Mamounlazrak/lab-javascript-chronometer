52eb977aba4f91e95c857b30c5aa57494342f842
Mamoun Lazrak <mamounlazrak@gmail.com>
2022-01-24 17:31:17 +0000

    lab done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,48 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0; 
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    if (!callback) {
+      this.intervalId = setInterval (() => {
+        this.currentTime++;
+      }, 1000) 
+    }
+    else { 
+      callback();
+    }
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let nbToString = value.toString(); 
+    if (nbToString.length == 1) {
+      nbToString = '0' + nbToString;
+      return nbToString;
+    } else {
+      return nbToString;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
 
